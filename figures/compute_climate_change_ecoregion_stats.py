448ab86bf8e253a7d963ae630074b36338635228
Nina <nina@tsf-476-wpa-7-151.epfl.ch>
2022-11-07 18:23:49 +0100

    compute sdm area no cropping

--- figures/compute_climate_change_ecoregion_stats.py
+++ figures/compute_climate_change_ecoregion_stats.py
@@ -1,26 +1,36 @@
 from config_figures import *
 
-def compute_species_stats_ecoregion(ecoid):
-    eco = ecoregions.filter(ee.Filter.eq('ECO_ID', ecoid)).first()
+# function to compute per ecoregion statistics of SDM changes when applied to present (1981-2010) and future (2071-2100 SSP 5.85) climate scenarios
+def compute_species_stats_ecoregion(ecoid):    
+    # get eco_geo by merging geometries of all ecoregion features with ECO_ID = ecoid (for some ECO_IDs there are mutliple features to take into account) and get ecoregion area
     eco_geo = ecoregions.filter(ee.Filter.eq('ECO_ID', ecoid)).geometry()
     eco_area = ee.Image.pixelArea().reduceRegion(reducer = ee.Reducer.sum(), geometry = eco_geo, scale = scale_to_use, maxPixels = 1e13).getNumber('area')
 
+    #eco = ecoregions.filter(ee.Filter.eq('ECO_ID', ecoid)).first()
+    
+    # reduce species pool by selecting species whose SDM bounding box intersects with eco_geo
     species = sdm_bboxes.filterBounds(eco_geo).aggregate_array('species')
+    # reduce species pool further by selecting those that have at least one non zero pixel in the ecoregion for present or future SDM projections
     sdms_eco = sdms.filter(ee.Filter.inList('system:index', species)).map(lambda sdm: sdm.select(['covariates_1981_2010', 'covariates_2071_2100_ssp585'])).map(
         lambda sdm: sdm.set(sdm.rename(['in_eco_present', 'in_eco_future']).reduceRegion(
             reducer = ee.Reducer.anyNonZero(), geometry = eco_geo, scale = scale_to_use, maxPixels = 1e13
         ))
     ).filter(ee.Filter.Or(ee.Filter.eq('in_eco_present', 1), ee.Filter.eq('in_eco_future', 1)))
 
+    # compute SDM area in ecoregion for each species for present and future climate projections and relative change between both [(area_future - area_present) / area_present]
     sdm_area = sdms_eco.map(lambda sdm: sdm.set(sdm.multiply(ee.Image.pixelArea()).rename(['area_present','area_future']).reduceRegion(
         reducer = ee.Reducer.sum(), geometry = eco_geo, scale = scale_to_use, maxPixels = 1e13
     ))).map(lambda sdm: sdm.set('area_change', (sdm.getNumber('area_future').subtract(sdm.getNumber('area_present'))).divide(sdm.getNumber('area_present'))))
     
+    # compute median SDM area in ecoregion across species for present and future climate projections and median relative change in area
     median_area_present = sdm_area.filter(ee.Filter.eq('in_eco_present', 1)).aggregate_array('area_present').reduce(ee.Reducer.median())
     median_area_future = sdm_area.filter(ee.Filter.eq('in_eco_future', 1)).aggregate_array('area_future').reduce(ee.Reducer.median())
     median_area_change = sdm_area.filter(ee.Filter.eq('in_eco_present', 1)).aggregate_array('area_change').reduce(ee.Reducer.median())
   
-    return ee.Feature(None, eco.toDictionary(['BIOME_NAME','BIOME_NUM','ECO_ID','ECO_NAME','REALM']).combine({
+    # return feature (no geometry) with properties for biome name and number, eocregion ID and name, realm, ecoregion area, 
+    # number of species in ecoregion for present and future climate projections, number of species lost and gained between both climate projections,
+    # median SDM area across species for present and future climate projections, and median relative change in SDM area 
+    return ee.Feature(None, ecoregions.filter(ee.Filter.eq('ECO_ID', ecoid)).first().toDictionary(['BIOME_NAME','BIOME_NUM','ECO_ID','ECO_NAME','REALM']).combine({
         'ECO_AREA': eco_area,
         'n_present': sdms_eco.filter(ee.Filter.eq('in_eco_present', 1)).size(),
         'n_future': sdms_eco.filter(ee.Filter.eq('in_eco_future', 1)).size(),
@@ -49,4 +59,3 @@ if __name__ == '__main__':
     
     #ecoregions_species_numbers = ecoregions.map(get_ecoregion_numbers)
     #export_table_to_drive(ecoregions_species_numbers, 'ecoregions_species_numbers')
-
--- figures/config_figures.py
+++ figures/config_figures.py
@@ -1,7 +1,7 @@
-import os
-import pandas as pd
-import numpy as np 
-import matplotlib.pyplot as plt
+# import os
+# import pandas as pd
+# import numpy as np 
+# import matplotlib.pyplot as plt
 
 import ee
 try: ee.Initialize()
@@ -10,25 +10,27 @@ except: sys.exit('ERROR starting earthengine python API')
 google_drive_folder = 'treemap'
 google_drive_path = '~/Google Drive/My Drive/' + google_drive_folder
 
+earthengine_folder = 'users/ninavantiel/treemap/'
+
 sdms = ee.ImageCollection('projects/crowtherlab/nina/treemap/sdms_binary').filter(ee.Filter.gte('nobs',90))
 sdm_bboxes = ee.FeatureCollection('projects/crowtherlab/nina/treemap_figures/sdms_bbox')
 scale_to_use = sdms.first().projection().nominalScale()
 
-## Potential forest: Bastin et al. potential tree cover >= 10%
-potential_forest = ee.Image('projects/crowtherlab/nina/treemap_figures/bastin_potential_tree_cover').gte(10) 
-## Current forests: Hansen et al. tree cover in 2010 >= 10% (intersected with potential forests)
-current_forest = ee.Image('projects/crowtherlab/nina/treemap_figures/hansen_year2000').gte(10).And(potential_forest)
+# ## Potential forest: Bastin et al. potential tree cover >= 10%
+# potential_forest = ee.Image('projects/crowtherlab/nina/treemap_figures/bastin_potential_tree_cover').gte(10) 
+# ## Current forests: Hansen et al. tree cover in 2010 >= 10% (intersected with potential forests)
+# current_forest = ee.Image('projects/crowtherlab/nina/treemap_figures/hansen_year2000').gte(10).And(potential_forest)
 
-ecoregions = ee.FeatureCollection('projects/crowtherlab/nina/treemap/Ecoregions')
-biome_image = ecoregions.reduceToImage(['BIOME_NUM'], ee.Reducer.first())
-biome_dictionary = ee.Dictionary.fromLists(
-	ecoregions.distinct('BIOME_NUM').aggregate_array('BIOME_NAME'), 
-	ecoregions.distinct('BIOME_NUM').aggregate_array('BIOME_NUM')
-).remove(['N/A'])
+# ecoregions = ee.FeatureCollection('projects/crowtherlab/nina/treemap/Ecoregions')
+# biome_image = ecoregions.reduceToImage(['BIOME_NUM'], ee.Reducer.first())
+# biome_dictionary = ee.Dictionary.fromLists(
+# 	ecoregions.distinct('BIOME_NUM').aggregate_array('BIOME_NAME'), 
+# 	ecoregions.distinct('BIOME_NUM').aggregate_array('BIOME_NUM')
+# ).remove(['N/A'])
 
 unbounded_geo = ee.Geometry.Polygon([-180, 88, 0, 88, 180, 88, 180, -88, 0, -88, -180, -88], None, False)
 
-## Function masking SDM pixels equal to 0 and pixels that are less than 50% within the SDM range (clipped)
+# ## Function masking SDM pixels equal to 0 and pixels that are less than 50% within the SDM range (clipped)
 def mask_sdm(sdm): return sdm.mask(sdm.mask().gte(0.5)).selfMask()
 
 def export_table_to_drive(fc, filename):
@@ -36,5 +38,14 @@ def export_table_to_drive(fc, filename):
 		collection = fc,
 		description = filename,
 		folder = google_drive_folder
+	)
+	export.start()
+
+def export_table_to_asset(fc, filename):
+	export = ee.batch.Export.table.toAsset(
+		collection = fc,
+		description = filename,
+		assetId = earthengine_folder + filename
+
 	)
 	export.start()
